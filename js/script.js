4b43a48c2469f82d6c9c30ea27f5d86a19593d91
Anastasia Polozova <polozova.anastasi@gmail.com>
2022-01-11 19:20:18 +0300

    Refactoring + modal window + burger menu + new texts

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let modal = document.getElementById('myModal');
+
+let btns = document.querySelectorAll('.link--js');
+
+let span = document.getElementsByClassName("close")[0];
+
+
+btns.forEach(btn => {
+  btn.addEventListener('click', () => modal.style.display = "block");
+});
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
